50aaee99846ab078cb970fa6e88fadf224fde8a0
gonzalin <gonzalogre@hotmail.com>
2020-05-12 16:03:53 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.use(bodyParser.json());
+ 
+app.get('/user', function (req, res) {
+    res.json('get user');   
+});
+
+app.post('/user', function (req, res) {
+
+    let body = req.body;
+
+    if ( body.name === undefined) {
+        res.status(400).json({
+            ok: false,
+            messagge: 'name is necesary'
+        });
+    } else {
+        res.json({
+            body
+        });
+    }
+
+   
+});
+
+app.put('/user/:id', function (req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/user', function (req, res) {
+    res.json('delete user');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("listening on port 3000")
+});
